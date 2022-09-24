a13f88bbf93a95ba329741dcf40997e0d2c68b06
WENHAO <w1160730299@hotmail.com>
2022-09-24 21:50:41 +0900

    hf first commit

--- src/git_test/gitTest.java
+++ src/git_test/gitTest.java
@@ -2,7 +2,8 @@ package git_test;
 
 public class gitTest {
     public static void main(String[] args) {
+        System.out.println("aaaa");
         System.out.println("test");
-
+        System.out.println("test");
     }
 }
